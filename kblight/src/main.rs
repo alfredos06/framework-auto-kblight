d893104c09f142ae284218aa94bb9cc339e11711
alfredos06 <famdahlin4@gmail.com>
2026-04-12 00:18:23 +0200

    added gitignore and wrote functioning code, yay

--- kblight/src/main.rs
+++ kblight/src/main.rs
@@ -5,21 +5,34 @@ use std::fs;
 
 fn main() {
 
-    let file_path = "/sys/bus/iio/devices/iio:device0/in_illuminance_sampling_frequency";
+    let scale = 1;
+    let i32brightness: i32;
+    let file_path = "/sys/bus/iio/devices/iio:device0/in_illuminance_raw";
+    let path_to_brightness = "/sys/class/leds/chromeos::kbd_backlight/brightness";
 
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
+    let lumen = contents.trim().parse::<i32>().unwrap();
 
-    let path_to_brightness = "/sys/class/leds/chromeos::kbd_backlight/brightness";
+    if lumen > 70 {
+        i32brightness = 0;
+    } 
+    else if lumen <= 0 {
+        i32brightness = 100;
+    }else {
+        i32brightness = lumen * scale;
+    }
 
-    let res = fs::write(path_to_brightness, "100");
+    let brightness = &i32brightness.to_string();
+    
+    println!("{lumen}, {brightness}");
+
+    let res = fs::write(path_to_brightness, brightness);
 
     match res {
         Err(e) => println!("Fuckin Error {e}"),
         Ok(f) => f,
     }
-
-    println!("{contents}");
 
 }
